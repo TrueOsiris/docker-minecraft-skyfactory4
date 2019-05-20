11bcee402daee3f5587551c693185056944db157
Osiris <tim@chaubet.be>
2019-05-20 14:45:36 +0200

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+cd /data
+
+cp -rf /tmp/feed-the-beast/* .
+echo "eula=true" > eula.txt
+
+if [[ ! -e server.properties ]]; then
+    cp /tmp/server.properties .
+fi
+
+if [[ -n "$MOTD" ]]; then
+    sed -i "/motd\s*=/ c motd=$MOTD" /data/server.properties
+fi
+if [[ -n "$LEVEL" ]]; then
+    sed -i "/level-name\s*=/ c level-name=$LEVEL" /data/server.properties
+fi
+if [[ -n "$OPS" ]]; then
+    echo $OPS | awk -v RS=, '{print}' >> ops.txt
+fi
+
+java $JVM_OPTS -jar FTBserver-*.jar nogui
